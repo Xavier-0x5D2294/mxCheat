432dcd42c221c009649e3ae0f939f34a31b985e2
Adam <meow-m@users.noreply.github.com>
2015-08-15 22:09:52 +0100

    Changed ESP Font

--- mxCheat/esp.lua
+++ mxCheat/esp.lua
@@ -4,8 +4,8 @@ CreateClientConVar("mx_esp", 1)
 		for k,v in pairs(player.GetAll()) do
 		if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
 		local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen() 
-		draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
-		draw.SimpleTextOutlined(v:GetUserGroup(), "ChatFont", posi.x, posi.y - -5, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+		draw.SimpleTextOutlined(v:Nick(), "HudSelectionText", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+		draw.SimpleTextOutlined(v:GetUserGroup(), "HudSelectionText", posi.x, posi.y - -5, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
 		end
 	end
 end
