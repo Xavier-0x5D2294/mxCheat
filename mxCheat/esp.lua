1c03fb197dc6095fdfd66e621292611bc3407229
Xavier <xavierm4477@gmail.com>
2015-08-15 20:43:02 +0000

    Added indentation

--- mxCheat/esp.lua
+++ mxCheat/esp.lua
@@ -1,12 +1,12 @@
 CreateClientConVar("mx_esp", 1)
 	function ESP()
 		if GetConVarNumber("mx_esp") == 1 then
-		for k,v in pairs(player.GetAll()) do
-		if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
-		local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen() 
-		draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+			for k,v in pairs(player.GetAll()) do
+			if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
+			local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen() 
+			draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+			end
 		end
-end
-end
+	end
 end
 hook.Add("HUDPaint", "ESP", ESP)
