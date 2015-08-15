a5ff50133b6e090890f294824eab11bb225ffeca
Xavier <xavierm4477@gmail.com>
2015-08-15 18:30:38 +0000

    First shit done

--- mxCheat.lua
+++ mxCheat.lua
@@ -1,1 +1,19 @@
 // A Garry's Mod Lua cheat created by Xavier and Meow
+
+/*==============
+TO DO LIST
+==============
+ESP
+Bunny Hop
+180 Turn
+Entity ESP
+Aimbot
+Speedhack
+Convar Forcer
+Menu
+*/
+
+function CommandList()
+	print("some fuckin commands here")
+end
+concommand.Add("mx_commands", CommandList)
