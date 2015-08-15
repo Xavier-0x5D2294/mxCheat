1bbe1655093eeacb1fd667b93973aa11b5bba9f8
Xavier <xavierm4477@gmail.com>
2015-08-15 20:13:09 +0000

    Added ESP convar
    Format is horrible but it works fine.

--- mxCheat/esp.lua
+++ mxCheat/esp.lua
@@ -1,11 +1,12 @@
-function ESP()
-	for k,v in pairs(player.GetAll()) do
-		if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
+CreateClientConVar("mx_esp", 1)
+	if GetConVarNumber("mx_esp") == 1 then
+		function ESP()
+			for k,v in pairs(player.GetAll()) do
+			if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
 			local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen() 
 			draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
 		end
 	end
 end
 hook.Add("HUDPaint", "ESP", ESP)
-
--- Need to add more, EG display v:Nick below the box, etc, but for now, this is good
+else return end
