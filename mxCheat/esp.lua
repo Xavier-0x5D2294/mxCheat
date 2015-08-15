8a66fdc9f751234ed8acb10035bc464389c1b3ee
Meow <explodingmelon11@gmail.com>
2015-08-15 20:02:48 +0100

    Fixed errors and included name to be drawn

--- mxCheat/esp.lua
+++ mxCheat/esp.lua
@@ -1,7 +1,9 @@
 function ESP()
-	for k,v in pairs(Player.GetAll()) do
-		hook.Add("HUDPaint", "ESP", ESP)
+	for k,v in pairs(player.GetAll()) do
+		if v != LocalPlayer() and v:Alive() then
+			local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen()
+			draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+		end
 	end
 end
-
--- Do whatever with this shit
+hook.Add("HUDPaint", "ESP", ESP)
