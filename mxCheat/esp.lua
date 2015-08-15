8b13d889eeeaa9fed28031854d25ca0ff339bae5
Xavier <xavierm4477@gmail.com>
2015-08-15 20:04:19 +0000

    Added comments

--- mxCheat/esp.lua
+++ mxCheat/esp.lua
@@ -1,9 +1,11 @@
 function ESP()
 	for k,v in pairs(player.GetAll()) do
-		if v != LocalPlayer() and v:Alive() then
-			local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen()
+		if v != LocalPlayer() and v:Alive() then  -- Checks if player is alive
+			local posi = ( v:GetPos() + Vector( 0,0,80 ) ):ToScreen() 
 			draw.SimpleTextOutlined(v:Nick(), "ChatFont", posi.x, posi.y - 10, Color(255,97,244), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
 		end
 	end
 end
 hook.Add("HUDPaint", "ESP", ESP)
+
+-- Need to add more, EG display v:Nick below the box, etc, but for now, this is good
